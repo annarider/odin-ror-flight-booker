cc41a18cd9dfa92bc1ef56725ef232c366032857
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-23 14:27:31 -0600

    fix: routes, missing : in :index
    - missing colon in :index
    - remove redundant get since there's already
    root 'flights#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "flights/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,5 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   root 'flights#index'
 
-  resources :flights, only: [index]
+  resources :flights, only: [:index]
 end
