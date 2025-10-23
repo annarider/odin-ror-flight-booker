348d17f5a4c3997ad79f3a3c65bc03d26bf2f920
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-23 10:10:09 -0600

    feat: add relationship to Airport model
    - add corresponding relationships from child M:M
    flights to Airport model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,3 +1,6 @@
 class Airport < ApplicationRecord
   validates :code, presence: true, uniqueness: true, length: { is: 3 }
+
+  has_many :flights_from, class_name: 'Flight', foreign_key: 'airport_from_id'
+  has_many :flights_to, class_name: 'Flight', foreign_key: 'airport_to_id'
 end
