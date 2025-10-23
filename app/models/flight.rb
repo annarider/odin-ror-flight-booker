0ce2966b281d79d2ef09f772bfeaaa9b372e3706
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-23 10:13:37 -0600

    feat: add validations on Flight model
    - make sure start is not null
    - make sure the departure and arrival airports
    are not the same

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,15 @@
 class Flight < ApplicationRecord
   belongs_to :airport_from, class_name: 'Airport', foreign_key: 'airport_from_id'
   belongs_to :airport_to, class_name: 'Airport', foreign_key: 'airport_to_id'
+
+  validates :start, :duration, presence: true
+  validate :airport_from_not_equal_airport_to
+
+  private
+
+  def airport_from_not_equal_airport_to
+    if airport_from_id == airport_to_id
+      errors.add(:airport_to, "can't be the same as departure airport")
+    end
+  end
 end
