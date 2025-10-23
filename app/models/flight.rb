43735b7cae6980310e4fb7f7a075a2a59a9af040
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-23 10:07:41 -0600

    feat: add foreign key info to Flight model
    - make the foreign key names more relevant to
    the business domain

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ApplicationRecord
-  belongs_to :airport_from
-  belongs_to :airport_to
+  belongs_to :airport_from, class_name: 'Airport', foreign_key: 'airport_from_id'
+  belongs_to :airport_to, class_name: 'Airport', foreign_key: 'airport_to_id'
 end
