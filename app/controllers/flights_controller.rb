26d10fe3b08872660862a8bbd60c7ee7071681c0
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-24 14:31:42 -0600

    feat: add FlightsController date restriction var
    - add variable available_dates to extract unique
    flight dates
    - use to populate the date dropdown in the
    Flights#index view's search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
+    @available_dates = Flight.pluck(:start).map(&:to_date).uniq.sort
     @flights = Flight.all
   end
 end
