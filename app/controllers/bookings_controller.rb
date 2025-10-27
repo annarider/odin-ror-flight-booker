53069d89213bbc70e7ebcd7df6ee98134d0956d8
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 17:04:16 -0600

    feat: add new BookingsController#create action
    - add logic for #create action
    - make sure nested attributes works
    - use strong params for security
    - keep track of the flight_id in case the create
    failed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,25 @@ class BookingsController < ApplicationController
 
     # Create blank passenger objects
     params[:num_passengers].to_i.times { @booking.passengers.build }
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was created.'
+    else
+      @flight = Flight.find(params[:booking][:flight_id])
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :flight_id,
+      :number_of_passengers,
+      passengers_attributes: [:name])
   end
 end
