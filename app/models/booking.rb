4c09fd64df8b84c43159cd2aacc848c4ee6badb1
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 13:48:36 -0600

    fix: Booking model accept nested attributes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers, dependent: :destroy
+  accepts_nested_attributes_for :passengers
 
   validates :number_of_passengers, presence: true,
             numericality: { only_integer: true, greater_than: 0 }
