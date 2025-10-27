36fb322b91b3c86d9371e2583c399a5a6170f905
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 13:23:14 -0600

    feat: create Passenger model and migration

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
   belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
 
+  has_many :bookings, dependent: :destroy
+
   validates :start, :duration, presence: true
   validate :departure_airport_not_equal_arrival_airport
 
---
+++ db/migrate/20251027192310_create_passengers.rb
@@ -0,0 +1,10 @@
+class CreatePassengers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :passengers do |t|
+      t.references :booking, null: false, foreign_key: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
