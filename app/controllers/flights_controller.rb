960f33e2a549cb6a1fa816248508676a8e83cf94
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 10:37:32 -0600

    feat: add logic to show flights searched for
    - change logic that's displaying all flights to
    only showing flights that meet the search criteria
    - check all search criteria are present
    - then conduct search if criteria are all present

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,22 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @available_dates = Flight.available_dates
-    @flights = Flight.all
+    @flights = search_params_present? ? search_flights : []
+  end
+
+  private
+
+  def search_params_present?
+    params[:departure_airport_id].present? &&
+      params[:arrival_airport_id].present? &&
+      params[:start].present?
+  end
+
+  def search_flights
+    Flight.search(
+      params[:departure_airport_id],
+      params[:arrival_airport_id],
+      params[:start]
+    )
   end
 end
