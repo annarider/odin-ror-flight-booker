9f20911b9ac09495256a1f20ea4853acb2dc1818
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 10:03:57 -0600

    refactor: @available_dates to use database more
    - use database for processing data instead of
    relying on Ruby in memory
    - database does distinct instead of Ruby uniq
    - database sorts instead of Ruby sorting

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @available_dates = Flight.pluck(:start).map(&:to_date).uniq.sort
+    @available_dates = Flight.select(:start).distinct.order(:start).pluck(:start).map(&:to_date)
     @flights = Flight.all
   end
 end
