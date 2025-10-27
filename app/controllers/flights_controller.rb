1900f5e5859e60d029f8749ce1e5c56311e67713
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 10:29:33 -0600

    fix: query to distinct on date, not date time
    - the time in the start column makes the datetimes
    look unique
    - when only casting to date, the duplicate dates
    disappear
    - use Arel.sql to create raw SQL queries to address
    gap in ActiveRecord API

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,12 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @available_dates = Flight.select(:start).distinct.order(:start).pluck(:start).map(&:to_date)
+    @available_dates = Flight
+      .select('DATE(start) as date')
+      .distinct
+      .order('DATE(start)')
+      .pluck(Arel.sql'DATE(start)')
+      .map(&:to_date)
     @flights = Flight.all
   end
 end
