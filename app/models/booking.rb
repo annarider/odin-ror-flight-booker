8702e66d0cd5ff101b26ea9224cc342df47a0ee6
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 13:21:17 -0600

    feat: add association & validation to model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :flight
+  has_many :passengers, dependent: :destroy
+
+  validates :number_of_passengers, presence: true,
+            numericality: { only_integer: true, greater_than: 0 }
 end
