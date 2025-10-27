5e42923a7b42621d1e046458f4dd072535e0b996
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-27 13:46:32 -0600

    feat: add #new action to BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new(flight: @flight, number_of_passengers: params[:num_passengers])
+
+    # Create blank passenger objects
+    params[:num_passengers].to_i.times { @booking.passengers.build }
+
+  end
 end
