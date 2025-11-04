6554ae2f219851af1e04ea72550a247620f76d5c
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-11-03 17:52:46 -0700

    feat: add delete action to stimulus controller

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -9,4 +9,22 @@ export default class extends Controller {
     const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
     this.containerTarget.insertAdjacentHTML('beforeend', content)
   }
+
+  removePassenger(event) {
+    event.preventDefault()
+
+    const passengerField = event.target.closest('[data-nested-form-target="passenger"]')
+
+    // Check if this is an existing record (has a persisted ID)
+    const destroyInput = passengerField.querySelector('input[name*="_destroy"]')
+
+    if (destroyInput) {
+      // Mark for deletion instead of removing from DOM
+      destroyInput.value = '1'
+      passengerField.style.display = 'none'
+    } else {
+      // Remove from DOM if it's a new record
+      passengerField.remove()
+    }
+  }
 }
