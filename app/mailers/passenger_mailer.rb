ce92c66d43509ff539ecae54fab1d2a128a4982f
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-11-05 10:42:24 -0700

    feat: add confirmation_email method to mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: "notifications@flightbooker.com"
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @url = 'http://localhost:3000/'
+    mail(to: @user.email, subject: "Flight Confirmation")
+  end
 end
